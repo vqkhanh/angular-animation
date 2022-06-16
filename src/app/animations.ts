552f058ff9e5dc640bd5ced0b973721c6ace748a
vqkhanh2009@gmail.com <vqkhanh2009@gmail.com>
2022-06-16 13:35:04 +0700

    assignment slide

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -11,3 +11,14 @@ export let fade = trigger('fade', [
     ]),
 
   ]);
+
+  export let slide = trigger('slide', [
+    transition(':enter', [
+        style({ transform: 'translateX(-10px)' }),
+        animate(500)
+    ]),
+
+    transition(':leave', [
+        animate(500, style({ transform: 'translateX(-100%)'}))
+    ])
+  ]);
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { fade } from './../animations';
+import { fade, slide } from './../animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
   animations: [
-    fade
+    slide
     
   ]
 })
